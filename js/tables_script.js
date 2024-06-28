54bcd594669f8151bac64b6c0478037502aaa029
Sharath083 <133197992+Sharath083@users.noreply.github.com>
2024-06-28 11:57:56 +0530

    2

--- js/tables_script.js
+++ js/tables_script.js
@@ -11,3 +11,40 @@ if (document.body.classList.contains('dark-mode')) {
     button.style.color = 'white';
 }
 };
+
+// function timeFun(){
+//     const button=document.getElementById("time");
+//     if(button.textContent.contains("India")){
+//         button.textContent="";
+//     }else{
+//         button.textContent=Date();
+//     }
+// }
+function timeFun() {
+    const button = document.getElementById("time");
+    if (button.textContent.includes("India")) {
+        button.textContent = "";
+    } else {
+        button.textContent = new Date();
+    }
+}
+
+function stick(pos){
+    var pic;
+    if(pos=='jump'){
+        pic="../images/sJump.webp"
+    }
+    else if(pos=='dance'){
+        pic="../images/sDance.webp"
+    }
+    else if(pos=='run'){
+        pic="../images/sRunn.webp"
+    }
+    else if(pos=='fight'){
+        pic="../images/sFight.webp"
+    }
+    else{
+        pic="../images/sStart.webp"
+    }
+    document.getElementById("stickman").src=pic;
+}
